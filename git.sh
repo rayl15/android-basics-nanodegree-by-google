472cdfaff8072bf9338a66e8078046b2a2fd0a2c
Rahul Sharma <er.csrahul@gmail.com>
2018-04-03 16:41:39 +0530

    fix variable
    fix variable

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
-export commit = $(git log -1 | grep "commit" | cut -d " " -f 2)
-export author = $(git log -1 | grep "Author" | cut -d " " -f 2)
-export date = $(git log -1 | grep "Date") 
-export message = $(git log -1 | awk 'NR==1{next}NR==2{next}NR==3{next}NR==4{next}1')    
+commit = $(git log -1 | grep "commit" | cut -d " " -f 2)
+author = $(git log -1 | grep "Author" | cut -d " " -f 2)
+date = $(git log -1 | grep "Date") 
+message = $(git log -1 | awk 'NR==1{next}NR==2{next}NR==3{next}NR==4{next}1')    
 
 curl -X POST 'https://api.newrelic.com/v2/applications/108424649/deployments.json' \
      -H 'X-Api-Key:a072c0beaab9c91f77820ceaec726562' -i \
